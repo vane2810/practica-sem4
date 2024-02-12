e260631a0c2b1ff7b9f231f9bd39c063c50b364c
vane2810 <olgavanessasf@gmail.com>
2024-02-12 00:07:33 -0600

    semana4

--- elementos.js
+++ elementos.js
@@ -1,40 +1,52 @@
-document.addEventListener("DOMContentLoaded", function() {
+// Evento click
+const boton1 = document.getElementById("boton1");
+const boton2 = document.getElementById("boton2");
+const boton3 = document.getElementById("boton3");
+const mensaje1 = document.getElementById("mensaje1");
+ 
+boton1.addEventListener("click",()=>{
+    mensaje1.innerHTML=input1.value;  
+})
 
-    document.getElementById("box1").addEventListener("click", function() {
-        alert("Has hecho clic en el cuadro 1");
-    });
+boton2.addEventListener("click",()=>{
+    alert(input2.value);
+})
 
-    document.getElementById("box2").addEventListener("click", function() {
-        alert("Has hecho clic en el cuadro 2");
-    });
+boton3.addEventListener("click",()=>{
+    console.log(input3.value);
+});
 
-    document.getElementById("box3").addEventListener("click", function() {
-        alert("Has hecho clic en el cuadro 3");
-    });
+// Evento focus
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
+const input3 = document.getElementById("input3");
 
-    
-    document.getElementById("input1").addEventListener("focus", function() {
-        console.log("Input 1 enfocado");
-    });
+input1.addEventListener("focus", ()=>{
+    input1.setAttribute("style","background-color:white");
+});
+        
+input2.addEventListener("focus", ()=>{
+    input2.setAttribute("style","background-color:white");
+});
 
-    document.getElementById("input2").addEventListener("focus", function() {
-        console.log("Input 2 enfocado");
-    });
+input3.addEventListener("focus", function() {
+    input3.setAttribute("style","background-color:white");
+});
 
-    document.getElementById("input3").addEventListener("focus", function() {
-        console.log("Input 3 enfocado");
-    });
+// Evento mouseover
+const elemento1 = document.getElementById("elemento1");
+const elemento2 = document.getElementById("elemento2");
+const elemento3 = document.getElementById("elemento3");
+const descripcion = document.getElementById("mensaje3");
 
+elemento1.addEventListener("mouseover", () => {
+    descripcion.textContent = "Se dispara cuando se hace clic en un elemento HTML";
+});
 
-    document.getElementById("box4").addEventListener("mouseover", function() {
-        console.log("Mouse sobre el cuadro 4");
-    });
+elemento2.addEventListener("mouseover", function() {
+    descripcion.textContent = "Se dispara cuando un elemento HTML se selecciona o se enfoca";
+});
 
-    document.getElementById("box5").addEventListener("mouseover", function() {
-        console.log("Mouse sobre el cuadro 5");
-    });
-
-    document.getElementById("box6").addEventListener("mouseover", function() {
-        console.log("Mouse sobre el cuadro 6");
-    });
+elemento3.addEventListener("mouseover", function() {
+    descripcion.textContent = "Se dispara cuando el cursor del mouse se mueve sobre un elemento HTML";
 });
